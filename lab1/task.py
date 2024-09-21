f66f9011366dbd666f7bb2ec70d15e329c783ac5
Andrew <andrew.shyika@gmail.com>
2024-09-21 13:18:20 +0300

    Add extra task

---
+++ lab1/task.py
@@ -0,0 +1,24 @@
+import requests
+from math import sin
+
+url = "https://raw.githubusercontent.com/skrix/cv/main/cv.tex"
+response = requests.get(url)
+print(response.text)
+
+game = "Sleeping Dogs"
+print(game)
+
+is_student = False
+is_teacher = True
+print(is_student and is_teacher)
+print(is_student or is_teacher)
+
+is_raining = False
+is_snowing = False
+print(not is_raining)
+print(not is_snowing)
+
+x = 1.22
+y = 3.21
+result = ((x * y)**(1/3) + 13 * (x - y)**4 - x/y) * sin(x)
+print(result)
